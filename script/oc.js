ce326f3ae99b7de88e74c7306373e4b49a0b4195
narelim <jelove0776@gmail.com>
2025-03-26 00:38:33 +0900

    ㅅㅈ

--- script/oc.js
+++ script/oc.js
@@ -1,22 +1,6 @@
-function selectSubcategory(sub, category){
-    document.querySelectorAll('.subcategory span').forEach(el => {
-    el.classList.remove('selected');
-  });
-}
-
-const selected=document.getElementById(`${category}-${sub}`);
-if(selected)selected.classList.add(`selected`)
-
-    document.querySelectorAll(`.categorybig`).forEach(el=>{
-        el.classList.remove('selected');
-    })
-
-    const title=document.querySelector(`#${category}-title .categorybig`);
-    if(title)title.classList.add('selected');
-
-function toggleCategory(category) {
-    const allSubcategories = document.querySelectorAll('.subcategory');
-    const allHearts = document.querySelectorAll('.category-item .littleheart');
+ function toggleCategory(category) {
+        const allSubcategories = document.querySelectorAll('.subcategory');
+        const allHearts = document.querySelectorAll('.category-item .littleheart');
   
     // 모두 닫고, 모든 하트를 비우기
     allSubcategories.forEach(ul => ul.classList.remove('open'));
@@ -33,4 +17,20 @@ function toggleCategory(category) {
         if (currentHeart) currentHeart.textContent = '♥'; // 하트 색칠
       }
     }
-  }
+    }
+
+    function selectSubcategory(sub, category){
+        document.querySelectorAll('.subcategory span').forEach(el => {
+        el.classList.remove('selected');
+    });
+
+    const selected=document.getElementById(`${category}-${sub}`);
+    if(selected)selected.classList.add(`selected`)
+
+        document.querySelectorAll(`.categorybig`).forEach(el=>{
+            el.classList.remove('selected');
+        })
+
+        const title=document.querySelector(`#${category}-title .categorybig`);
+        if(title)title.classList.add('selected');
+    }
